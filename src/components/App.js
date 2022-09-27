f5f85544ced6f571f29c61f231b6e907c291ab27
shannone <shannone426@gmail.com>
2022-09-27 08:16:30 -0400

    render all plants

--- src/components/App.js
+++ src/components/App.js
@@ -12,3 +12,10 @@ function App() {
 }
 
 export default App;
+
+// Core Deliverables
+// As a user:
+// -When the app starts, I can see all plants.
+// -I can add a new plant to the page by submitting the form.
+// -I can mark a plant as "sold out".
+// -I can search for plants by their name and see a filtered list of plants.
