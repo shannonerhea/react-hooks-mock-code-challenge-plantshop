eeac226454c5281cdeb39310d6e027a5106e38be
shannone <shannone426@gmail.com>
2022-09-27 10:48:24 -0400

    render all plants

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function PlantCard() {
+// passing one plant as a prop
+function PlantCard({ plant }) {
+  //console.log(plant);
+  const { name, image, price } = plant;
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,11 +4,12 @@ import PlantCard from "./PlantCard";
 
 // passing prop of plants from plant pg to have cards show up 
 // mapping over then to render each card individually
+// inside PlantCard pass down plants var to access data for card img name price
 function PlantList({ plants }) {
   return (
     <ul className="cards">
       {plants.map((plant) => {
-      return <PlantCard />;
+      return <PlantCard key={plant.id} plant={plant} />;
     })}
     </ul>
   );
