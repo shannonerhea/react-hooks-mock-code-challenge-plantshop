6233d670dfcd509378ae834f6d7e70bdbb80bfeb
shannone <shannone426@gmail.com>
2022-10-05 19:28:46 -0400

    again

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
   //console.log(plant)
   const { image, name, price } = plant;
+  const [inStock, setInStock] = useState("false")
+
+  function toggleStock() {
+    setInStock((inStock) => !inStock)
+  }
+
 
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button onClick={toggleStock} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={toggleStock}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -6,6 +6,8 @@ import Search from "./Search";
 function PlantPage() {
   // i set state for plants
   const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+
 // fetch all plants data and set plants 
   useEffect(() => {
     fetch("http://localhost:6001/plants")
@@ -14,13 +16,23 @@ function PlantPage() {
       setPlants(plantData);
     })
   },[])
+
+  function handleAddPlant(newPlant) {
+    const updatePlantData = [...plants, newPlant];
+    setPlants(updatePlantData);
+  };
   //console.log({plants})
   // passing down plants onto PLantList below
+
+const displayedPlants = plants.filter((plant) => {
+  return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+  //pass this down to plants list plants={displayedPlants}
+});
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList plants={plants}/>
+      <NewPlantForm addPlant={handleAddPlant}/>
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+      <PlantList plants={displayedPlants}/>
     </main>
   );
 }
